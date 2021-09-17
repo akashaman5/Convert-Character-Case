fd9087c03b3d4f9f73d8ae57655ce210074f253e
Akash Aman <amanak@campbells.com>
2021-09-17 22:37:42 +0530

    Some Changes done:
    1. Added the logic to retry the input without actually restarting the application by calling the main() based on user input.
    
    2. Improved the console output formatting for better readability.
    
    3. customized the application title window :)
    
    Note: I have not implemented the try-catch logic for exception handling because we don't really need it at this point. The intended input(cosmos ID) is generally a mixture of Aplha numeric along with special characters in the middle.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,37 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace Convert_Character_Case
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.Title = "©Akash Aman";  //customize the app title window.
+
+
             string str1;
             char[] arr1;
             int l, i;
             l = 0;
             char ch;
-            Console.Write("\n\nReplace lowercase characters by uppercase and vice-versa :\n");
-            Console.Write("--------------------------------------------------------------\n");
-            Console.Write("Input the string : ");
+
+            Console.ForegroundColor = ConsoleColor.Blue;  //make the console output as blue.
+
+            Console.Write("\nReplace lower-case Cosmos ID by upper-case and vice-versa :\n");
+            Console.Write("--------------------------------------------------------------");
+            Console.Write("\n\nInput the Cosmos ID : ");
+
             str1 = Console.ReadLine();
             l = str1.Length;
             arr1 = str1.ToCharArray(0, l); // Converts string into char array.
 
-            Console.Write("\nAfter conversion, the string is : ");
+            Console.ForegroundColor = ConsoleColor.White;     //make the console output as white.
+            Console.Write("\nAfter the case conversion, Cosmos ID is: ");
             for (i = 0; i < l; i++)
             {
                 ch = arr1[i];
@@ -27,8 +40,29 @@ namespace Convert_Character_Case
                 else
                     Console.Write(Char.ToLower(ch)); // Converts uppercase character to lowercase.
             }
+
+            Console.ForegroundColor = ConsoleColor.Blue;  //make the console output blue again.
+
+            Console.WriteLine("\n\n\n\nDo that again?   Press 'y' and then hit ENTER\n"); //retry input without restarting the app.
+
+            String SelectedKey = Console.ReadLine();      //store the user's response for input retry.
+
+            if (SelectedKey == "y")
+            {
+                // clear the screen and reset the app by calling main method.
+
+                Console.Clear();
+                Main(args);
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;  //make the console output blue again.
+            Console.WriteLine("\n\nPress any key to exit....");
+
+            //Console.ReadKey();
+
             //Console.Write("\n\n");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
